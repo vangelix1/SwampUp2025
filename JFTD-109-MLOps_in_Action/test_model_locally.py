e6a50c7fe185b4c2e79df7fb4df714153111b230
williammanning <williammanning@gmail.com>
2025-08-22 16:20:19 -0700

    Updated Model
    I had to start again since the model was broken

--- JFTD-109-MLOps_in_Action/test_model_locally.py
+++ JFTD-109-MLOps_in_Action/test_model_locally.py
@@ -1,36 +1,27 @@
-from pandas import DataFrame
-#from qwak.model.tools import run_local
 from qwak.model.tools import run_local
 import json
-import os
+import pandas as pd
 
-from main import load_model
+from main import *
 
 if __name__ == '__main__':
     # Create a new instance of the model from __init__.py
     m = load_model()
 
-    # Define the columns
-    columns = ["prompt"]
 
-    #os.environ['TRAIN'] = True
+    df = pd.read_csv("main/small_fraud_dataset.csv", nrows=1)
 
-    # Define the data
-    data = [["How do I expose a deployment in Kubernetes using a service?"]]
-    
     # Create the DataFrame and convert it to JSON
-    df = DataFrame(data, columns=columns).to_json(orient="records")
-
-    print (df)
+    json_df = df.to_json()
     
     print("\n\nPREDICTION REQUEST:\n\n", df)
 
     # Run local inference using the model and print the prediction
     # The run_local function is part of the qwak library and allows for local testing of the model
-    prediction = run_local(m, df)
+    prediction = run_local(m, json_df)
     prediction_data = json.loads(prediction)
 
     # Extract the 'generated_text' value
-    generated_text = prediction_data
+    prediction_df = pd.DataFrame(prediction_data)
 
-    print (f"\n\nPREDICTION RESPONSE:\n\n{generated_text}")
+    print (f"\n\nPREDICTION RESPONSE:\n\n{prediction_df}")
